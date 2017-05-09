6d40ca3f0d78227c2811ff88d83c6438c66f8248
Melissa Choy <mel@otilas.com.au>
2017-05-09 21:07:31 +1000

    Add capital cities for states

--- lib/ausburbs/state.rb
+++ lib/ausburbs/state.rb
@@ -1,10 +1,29 @@
 module Ausburbs
   class State
-    attr_accessor :name, :suburbs
+    attr_accessor :name, :suburbs, :capital
 
     def initialize(*args)
       self.name = args[0]
       load_suburb_data!(args[1])
+      
+      case self.name        
+      when "NSW"
+        self.capital = "Sydney"
+      when "VIC"
+        self.capital =  'Melbourne'
+      when "ACT"
+        self.capital =  'Canberra'
+      when "QLD"
+        self.capital =  'Brisbane'
+      when "TAS"
+        self.capital =  'Hobart'
+      when "WA"
+        self.capital =  'Perth'
+      when "SA"
+        self.capital =  'Adelaide'
+      when "NT"
+        self.capital =  'Darwin'
+      end
     end
 
     private
